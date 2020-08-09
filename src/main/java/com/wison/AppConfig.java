2e2b58911bf54fdfefbe2aac21944371decd2080
weilisong <weilisong@ufenqi.com>
2020-08-09 16:09:54 +0800

    添加视图过滤器

--- src/main/java/com/wison/AppConfig.java
+++ src/main/java/com/wison/AppConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -13,6 +14,10 @@ import java.util.List;
 @ComponentScan("com.wison")
 @EnableWebMvc
 public class AppConfig implements WebMvcConfigurer {
+
+    public void configureViewResolvers(ViewResolverRegistry registry) {
+        registry.jsp("/",".jsp");
+    }
 
     public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
         System.out.println("configureMessageConverters -------");
